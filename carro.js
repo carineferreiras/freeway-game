f37280fbf14b3a23e589e3d4f8151053deda5542
Carine Polesso <111937653+carineferreiras@users.noreply.github.com>
2023-01-11 00:16:56 -0300

    Update carro.js

--- carro.js
+++ carro.js
@@ -16,11 +16,23 @@ let velocidadeCarro3=3.5
 
 function mostraCarro(){
   image (imagemCarro,xCarro,yCarro, 50, 40)
-  image (imagemCarro2,xCarro2,yCarro2,50,40)
-  image (imagemCarro3,xCarro3,yCarro3,50, 40)
+  image(imagemCarro2,xCarro2,yCarro2,50,40)
+  image(imagemCarro3,xCarro3,yCarro3,50,40)
 }
 function movimentaCarro(){
   xCarro-= velocidadeCarro1
   xCarro2 -= velocidadeCarro2
   xCarro3 -= velocidadeCarro3
+}
+
+function voltaPosicaoInicialDoCarro(){
+  if (xCarro <-50){
+    xCarro=600
+  }
+  if (xCarro2 <-50){
+    xCarro2=600
+  }
+  if (xCarro3 <-50){
+    xCarro3=600
+  }
 }
