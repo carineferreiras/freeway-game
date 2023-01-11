363bd0cf289543753adf6cc39fb36d3456873b3b
Carine Polesso <111937653+carineferreiras@users.noreply.github.com>
2023-01-11 01:30:12 -0300

    extra content

--- image.js
+++ image.js
@@ -2,9 +2,14 @@
 let imagemDaEstrada;
 let imagemDoAtor;
 let imagemCarro;
+let imagemCarro2;
+let imagemCarro3;
+
 
 function preload(){  imagemDaEstrada=loadImage("image/estrada.png")
   imagemDoAtor=loadImage("image/ator-1.png")
-  
   imagemCarro = loadImage("image/carro-1.png")
+  imagemCarro2 = loadImage("image/carro-2.png")
+  imagemCarro3 = loadImage("image/carro-3.png")
+  imagemCarros=[imagemCarro,imagemCarro2,imagemCarro3]
 }
