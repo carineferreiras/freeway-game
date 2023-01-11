8842119fb7b965e8ea9076b9844b113e46e4e2bd
Carine Polesso <111937653+carineferreiras@users.noreply.github.com>
2023-01-10 23:51:23 -0300

    new commit new structure

--- script.js
+++ script.js
@@ -1,18 +1,3 @@
-let imagemDaEstrada;
-let imagemDoAtor;
-let imagemCarro;
-//variaveis do carro
-let xCarro=600
-
-//ator
-let yAtor=366
-function preload(){
-  imagemDaEstrada=loadImage("image/estrada.png")
-  imagemDoAtor=loadImage("image/ator-1.png")
-  
-  imagemCarro = loadImage("image/carro-1.png")
-}
-
 function setup() {
   createCanvas(500, 400);
 }
@@ -23,24 +8,4 @@ function draw() {
   mostraCarro();
   movimentaCarro();
   movimentaAtor();
-}
-function mostraAtor(){
-  image(imagemDoAtor,100,yAtor, 30,30 )
-}
-
-function mostraCarro(){
-  image (imagemCarro,xCarro, 40, 50, 40)
-
-}
-function movimentaCarro(){
-  xCarro-= 2; 
-}
-
-function movimentaAtor(){
-  if (keyIsDown(UP_ARROW)){
-    yAtor -=3;
-  }
-  if (keyIsDown(DOWN_ARROW)){
-    yAtor +=3;   
-  }
 }
