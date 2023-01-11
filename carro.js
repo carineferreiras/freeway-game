7336f519498c91864ca5ed7241737ff79fee1ad0
Carine Polesso <111937653+carineferreiras@users.noreply.github.com>
2023-01-11 01:29:32 -0300

    clear code with use of for and i for variables

--- carro.js
+++ carro.js
@@ -1,28 +1,25 @@
 //codigo dos carros
-let yCarros=[40,96,150]
-let velocidadeCarros=[2,2.5,3.5]
-let xCarros=[600,600,600]
+let yCarros=[40,96,150];
+let velocidadeCarros=[2,2.5,3.5];
+let xCarros=[600,600,600];
 
 function mostraCarro(){
-  image (imagemCarro,xCarros[0],yCarros[0], 50, 40)
-  image(imagemCarro2,xCarros[1],yCarros[1],50,40)
-  image(imagemCarro3,xCarros[2],yCarros[2],50,40)
-  print(yCarros)
+  for(let i=0; i < imagemCarros.length ; i = i+1){
+  image(imagemCarros[i],xCarros[i],yCarros[i],50,40);
+  }
 }
 function movimentaCarro(){
-  xCarros[0]-= velocidadeCarros[0]
-  xCarros[1] -= velocidadeCarros[1]
-  xCarros[2] -= velocidadeCarros[2]
+  for (let i=0; i < imagemCarros.length ; i= i+1)
+  xCarros[i]-= velocidadeCarros[i];
 }
 
 function voltaPosicaoInicialDoCarro(){
-  if (xCarros[0] <-50){
-    xCarros[0]=600
+  for (let i=0; i < imagemCarros.length ; i= i+1)
+  if (passouTodaTela(xCarros[i])){
+    xCarros[i]=600;
   }
-  if (xCarros[1] <-50){
-    xCarros[1]=600
-  }
-  if (xCarros[2] <-50){
-    xCarros[2]=600
-  }
+}
+
+function passouTodaTela(xCarro){
+  return xCarros <-50
 }
