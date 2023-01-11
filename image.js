faa1eaccb6326cc1651312e037303f4ec762ca81
Carine Polesso <111937653+carineferreiras@users.noreply.github.com>
2023-01-10 23:50:44 -0300

    variables da imagem

---
+++ image.js
@@ -0,0 +1,10 @@
+//imagens
+let imagemDaEstrada;
+let imagemDoAtor;
+let imagemCarro;
+
+function preload(){  imagemDaEstrada=loadImage("image/estrada.png")
+  imagemDoAtor=loadImage("image/ator-1.png")
+  
+  imagemCarro = loadImage("image/carro-1.png")
+}
